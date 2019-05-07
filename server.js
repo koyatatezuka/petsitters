ef4b0d1b167563beba64691c4d8432b85e450e05
koyata tezuka <koytez@gmail.com>
2019-05-06 17:29:15 -1000

    mongoose init

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { mongoURI } = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(mongoURI, {
+		useNewUrlParser: true
+	})
+	.then(() => {
+		console.log('MongoDB connected...');
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
